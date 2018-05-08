dc45270801ccc3c3958c1133b7cfefeec19e34f4
Sylvain Bonnard <sylvain@okamidev.com>
2018-05-08 09:24:23 +0200

    Start - Vue CLI generated

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+    <h1>Philosophers list</h1>
+    <div class="lead mx-auto clearfix" style="width: 800px;height: 300px;">
+        <!--Add your philosophers list here-->
+    </div>
+  </div>
+</template>
+
+<script>
+  //Write your App component script here
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,54 +1,12 @@
-Vue.component('navbar', {
-    template: '<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">\n' +
-    '    <a class="navbar-brand" href="#">Navbar</a>\n' +
-    '    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault"\n' +
-    '            aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">\n' +
-    '        <span class="navbar-toggler-icon"></span>\n' +
-    '    </button>\n' +
-    '\n' +
-    '    <div class="collapse navbar-collapse" id="navbarsExampleDefault">\n' +
-    '        <ul class="navbar-nav mr-auto">\n' +
-    '            <li class="nav-item active">\n' +
-    '                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>\n' +
-    '            </li>\n' +
-    '        </ul>\n' +
-    '    </div>\n' +
-    '</nav>'
-});
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-Vue.component('philosopher', {
-    props: {
-        philosopher: {
-            type: Object,
-            default: null
-        }
-    },
-    template: '<div style="height:200px;width:200px" class="float-left m-3"><img :src="philosopher.img" class="img-thumbnail img-fluid"/><p>{{philosopher.name}}</p></div>'
-});
+Vue.config.productionTip = false
 
-var app = new Vue({
-    el: '#app',
-    data: function() {
-        return {
-            philosophers: [{
-                img: 'img/moss.jpg',
-                name: 'Maurice Moss'
-            },{
-                img: 'img/sheldon.jpg',
-                name: 'Sheldon Cooper'
-            },{
-                img: 'img/goldblum.jpg',
-                name: 'Jeff Goldblum'
-            }]
-        }
-    },
-    methods: {
-        showMessage: function() {
-            alert("Alert from Vue !!!")
-        }
-    }
-});
-
-document.getElementById("external-button").onclick = function () {
-    app.showMessage();
-};
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
